849e5d67a2aa6d656c00f3ceea60b8f58ebe77af
GitHub Action <action@github.com>
2022-12-24 04:24:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/chenbinandroid/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chenbinandroid/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chenbinandroid.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chenbinandroid.template.MyBundle
 
 class MyApplicationService {
 
